4114a688e475034ce96159402e653fde64005a65
lmjnels <lmjnelson@yahoo.com>
2024-01-22 02:49:26 +0000

    Add charset

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
   <head>
+      <meta charset="<?php bloginfo('charset')?>">
       <meta name="viewport" content="width=device-width, inital-scale=1">
       <?php wp_head(); ?>
   </head>
