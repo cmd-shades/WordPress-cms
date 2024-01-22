55f31155ec28796b6be63a11b8437d06237e6b90
lmjnels <lmjnelson@yahoo.com>
2024-01-22 01:52:31 +0000

    Add theme support to allow WordPress to control the title of a page

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -9,3 +9,10 @@ function university_files() {
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features(){
+    // Give WordPress title tag control
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
