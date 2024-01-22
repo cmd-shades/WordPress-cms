56872fdf0a0a3f3deb3d5c34d41b83a31e8f6e51
lmjnels <lmjnelson@yahoo.com>
2024-01-22 16:43:09 +0000

    Code cleanup
    - Add space between parenthesis and curly brace

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -10,7 +10,7 @@ function university_files() {
 
 add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features(){
+function university_features() {
     // Give WordPress title tag control
     add_theme_support('title-tag');
     // Set up navigation
