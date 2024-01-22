2d6161c2c8f4cc8fa409032bdff06c7c370765de
lmjnels <lmjnelson@yahoo.com>
2024-01-22 02:50:14 +0000

    Add body class

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -5,7 +5,7 @@
       <meta name="viewport" content="width=device-width, inital-scale=1">
       <?php wp_head(); ?>
   </head>
-  <body>
+  <body <?php body_class() ?>>
   <header class="site-header">
       <div class="container">
           <h1 class="school-logo-text float-left">
