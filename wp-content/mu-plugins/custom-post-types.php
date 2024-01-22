fc9e2c0678a35eaacf5f0608f694805febd2fd76
lmjnels <lmjnelson@yahoo.com>
2024-01-22 23:01:08 +0000

    Add additional support to array to custom post type
    - Ensure additonal post content is shown for editoring

--- wp-content/mu-plugins/custom-post-types.php
+++ wp-content/mu-plugins/custom-post-types.php
@@ -4,23 +4,23 @@ function university_post_types()
 {
     // @see https://developer.wordpress.org/reference/functions/register_post_type/
     register_post_type('event', [
-        'has_archive'   => true,
-        'public'         =>  true,
-        'labels'         =>  [
-            'name'           =>  'Events',
-            'singular_name'  =>  'Event',
-            'add_new'        =>  'Add New Event',
-            'add_new_item'   =>  'Add New Event',
-            'new_item'       =>  'New Event',
-            'edit_item'      =>  'Edit Event',
-            'view_item'      =>  'View Event',
-            'all_items'      =>  'All Events',
-            'search_items'      =>  'Search Events',
-        ],
-
-        'menu_icon'     =>  'dashicons-calendar-alt', // @see https://developer.wordpress.org/resource/dashicons/
-        'show_in_rest'  =>  true, // show Gutenberg editor
-    ]);
+		'show_in_rest' => true, // show Gutenberg editor
+		'supports'     => ['title', 'editor', 'excerpt',],
+		'has_archive'  => true,
+		'public'       => true,
+		'labels'       => [
+			'name'          => 'Events',
+			'singular_name' => 'Event',
+			'add_new'       => 'Add New Event',
+			'add_new_item'  => 'Add New Event',
+			'new_item'      => 'New Event',
+			'edit_item'     => 'Edit Event',
+			'view_item'     => 'View Event',
+			'all_items'     => 'All Events',
+			'search_items'  => 'Search Events',
+		],
+		'menu_icon'    => 'dashicons-calendar-alt', // @see https://developer.wordpress.org/resource/dashicons/
+	]);
 }
 
 add_action('init', 'university_post_types');
