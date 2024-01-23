fe77303676f49630ce385699222c1324dd5d6d9b
lmjnels <lmjnelson@yahoo.com>
2024-01-23 19:44:00 +0000

    Update "register_post_type" using rewrite for "events"

--- wp-content/mu-plugins/custom-post-types.php
+++ wp-content/mu-plugins/custom-post-types.php
@@ -6,11 +6,11 @@ function university_post_types()
     register_post_type('event', [
         'show_in_rest' => true, // show Gutenberg editor
         'supports'     => ['title', 'editor', 'excerpt'],
+        'rewrite'      => ['slug' => 'events'],
         'has_archive'  => true,
         'public'       => true,
         'labels'       => [
             'name'          => 'Events',
-            'singular_name' => 'Event',
             'add_new'       => 'Add New Event',
             'add_new_item'  => 'Add New Event',
             'new_item'      => 'New Event',
@@ -18,6 +18,7 @@ function university_post_types()
             'view_item'     => 'View Event',
             'all_items'     => 'All Events',
             'search_items'  => 'Search Events',
+            'singular_name' => 'Event',
         ],
         'menu_icon'    => 'dashicons-calendar-alt', // @see https://developer.wordpress.org/resource/dashicons/
     ]);
